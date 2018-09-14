f5c7fa766ec519fb20d2d98489c5d019ed561111
yts <sky877kr@gmail.com>
2018-09-14 17:02:09 +0900

    1.호출 Layout 처리 완료

--- app/src/main/java/todday/funny/seoulcatcher/service/MyFirebaseMessagingService.java
+++ app/src/main/java/todday/funny/seoulcatcher/service/MyFirebaseMessagingService.java
@@ -1,30 +1,98 @@
 package todday.funny.seoulcatcher.service;
 
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
+import com.bumptech.glide.request.target.NotificationTarget;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 
-import todday.funny.seoulcatcher.util.NotificationCreate;
+import java.util.HashMap;
+import java.util.Map;
+
+import todday.funny.seoulcatcher.GlideApp;
+import todday.funny.seoulcatcher.R;
+import todday.funny.seoulcatcher.model.Call;
+import todday.funny.seoulcatcher.ui.activity.IntroActivity;
+import todday.funny.seoulcatcher.util.DateFormat;
+
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private final static int NOTIFICATION_ID = 111;
 
-    private Handler handler = null;
+    private static Handler handler = null;
     private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
 
     @Override
     public void onMessageReceived(final RemoteMessage remoteMessage) {
         Log.d(TAG, ": " + remoteMessage.getFrom());
-        if (handler != null) {
-            handler.removeCallbacksAndMessages(null);
-        }
         if (remoteMessage.getData().size() > 0) {
-            NotificationCreate.startNotify(MyFirebaseMessagingService.this, remoteMessage.getData(), handler);
+            onDestroy();
+            startNotify(this, remoteMessage.getData());
         }
     }
+
+    public static void startNotify(final Context context, Map<String, String> data) {
+        Gson gson = new Gson();
+        JsonElement jsonElement = gson.toJsonTree(data);
+        final Call call = gson.fromJson(jsonElement, Call.class);
+        Intent intent = new Intent(context, IntroActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.notification_layout);
+        remoteViews.setTextViewText(R.id.text_time, DateFormat.getCallTime(System.currentTimeMillis()));
+
+
+        String name = context.getString(R.string.call_user_name, call.getToUserName()) + " / " + call.getKind();
+        if (call.getKind().equals(context.getString(R.string.cardiac_arrest))) {
+            name += " / " + call.getAge();
+        }
+        remoteViews.setTextViewText(R.id.text_to_user_name, name);
+
+        String location = context.getString(R.string.location) + " : " + call.getAddress();
+        remoteViews.setTextViewText(R.id.text_location, location);
+
+        final Notification notification = new NotificationCompat.Builder(context, context.getString(R.string.notification_channel_id))
+                .setSmallIcon(R.drawable.ic_bell_64px)
+                .setContentIntent(pendingIntent)
+                .setCustomContentView(remoteViews)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_STATUS)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+
+        handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                final NotificationTarget userTarget = new NotificationTarget(context, R.id.image_touser, remoteViews, notification, NOTIFICATION_ID);
+                GlideApp.with(context.getApplicationContext()).asBitmap().circleCrop().load(call.getToUserPhotoUrl()).into(userTarget);
+            }
+        });
+
+    }
+
 
     @Override
     public void onDestroy() {
@@ -32,6 +100,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         super.onDestroy();
         if (handler != null) {
             handler.removeCallbacksAndMessages(null);
+            handler = null;
         }
     }
 }
