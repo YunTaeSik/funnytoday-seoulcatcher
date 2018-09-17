b97297f61e2c9586bf40772a37d5b984b62190f2
yts <sky877kr@gmail.com>
2018-09-17 17:03:57 +0900

    1.맵작업중

--- app/src/main/java/todday/funny/seoulcatcher/viewmodel/MapViewModel.java
+++ app/src/main/java/todday/funny/seoulcatcher/viewmodel/MapViewModel.java
@@ -1,19 +1,36 @@
 package todday.funny.seoulcatcher.viewmodel;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapViewModel extends BaseViewModel implements OnMapReadyCallback {
+    private float MAP_ZOOM_LEVEL_WORLD = 1.0f;
+    private float MAP_ZOOM_LEVEL_LANDMASS_CONTINENT = 5.0f;
+    private float MAP_ZOOM_LEVEL_CITY = 10.0f;
+    private float MAP_ZOOM_LEVEL_STREETS = 15.0f;
+    private float MAP_ZOOM_LEVEL_STREETS_BULDINGS = 17.5f;
+    private float MAP_ZOOM_LEVEL_BUILDINGS = 20.0f;
+
     private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public MapViewModel(Context context) {
         super(context);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+
     }
 
     public void setMapView(MapView mapView) {
@@ -22,12 +39,24 @@ public class MapViewModel extends BaseViewModel implements OnMapReadyCallback {
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            map.setMyLocationEnabled(true);
-            return;
-        }
+        map.setMyLocationEnabled(true);
+        getCurrentLocation();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (map != null) {
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), MAP_ZOOM_LEVEL_STREETS_BULDINGS));
+                }
+            }
+        });
+
     }
 }
