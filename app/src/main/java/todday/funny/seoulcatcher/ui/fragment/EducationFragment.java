f7426c91e53d8de5463533a07663ae156f738f2c
yts <sky877kr@gmail.com>
2018-09-20 13:48:05 +0900

    1.교육 뷰

--- app/src/main/java/todday/funny/seoulcatcher/ui/fragment/EducationFragment.java
+++ app/src/main/java/todday/funny/seoulcatcher/ui/fragment/EducationFragment.java
@@ -28,19 +28,11 @@ import todday.funny.seoulcatcher.databinding.EducationBinding;
 import todday.funny.seoulcatcher.util.HeartSpeech;
 import todday.funny.seoulcatcher.viewmodel.EducationViewModel;
 
-public class EducationFragment extends Fragment implements View.OnClickListener{
+public class EducationFragment extends Fragment {
 
     private EducationBinding binding = null;
     private EducationViewModel model = null;
 
-    private ConstraintLayout emerLayout;
-    private ConstraintLayout disasterLayout;
-    private ConstraintLayout accidentLayout;
-    private ConstraintLayout layout199;
-
-    private ExpandableLayout expandable_emerLayout;
-    private ExpandableLayout expandable_disasterLayout;
-    private ExpandableLayout expandable_accidentLayout;
 
     public static EducationFragment newInstance() {
         Bundle args = new Bundle();
@@ -57,68 +49,7 @@ public class EducationFragment extends Fragment implements View.OnClickListener{
             model = ((BaseActivity) getActivity()).getEducationViewModel();
             binding.setModel(model);
         }
-        View view = binding.getRoot();
-
-        init(view);
-
-        return view;
+        return binding.getRoot();
     }
 
-    private void init(View view){
-        emerLayout = view.findViewById(R.id.layout_emergency);
-        disasterLayout = view.findViewById(R.id.layout_disaster);
-        accidentLayout = view.findViewById(R.id.layout_accident);
-        layout199 = view.findViewById(R.id.layout_119);
-
-        expandable_emerLayout = view.findViewById(R.id.expandable_layout_1);
-        expandable_disasterLayout = view.findViewById(R.id.expandable_layout_2);
-        expandable_accidentLayout = view.findViewById(R.id.expandable_layout_3);
-
-        expandable_emerLayout.collapse();
-        expandable_disasterLayout.collapse();
-        expandable_accidentLayout.collapse();
-
-        emerLayout.setOnClickListener(this);
-
-        disasterLayout.setOnClickListener(this);
-
-        accidentLayout.setOnClickListener(this);
-        accidentLayout.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.layout_emergency:
-                if(expandable_emerLayout.isExpanded())
-                    expandable_emerLayout.collapse();
-                else {
-                    expandable_accidentLayout.collapse();
-                    expandable_disasterLayout.collapse();
-                    expandable_emerLayout.expand();
-                }
-                break;
-            case R.id.layout_disaster:
-                if(expandable_disasterLayout.isExpanded())
-                    expandable_disasterLayout.collapse();
-                else {
-                    expandable_emerLayout.collapse();
-                    expandable_accidentLayout.collapse();
-                    expandable_disasterLayout.expand();
-                }
-                    break;
-            case R.id.layout_accident:
-                if(expandable_accidentLayout.isExpanded())
-                    expandable_accidentLayout.collapse();
-                else {
-                    expandable_disasterLayout.collapse();
-                    expandable_emerLayout.collapse();
-                    expandable_accidentLayout.expand();
-                }
-                break;
-            case R.id.layout_119:
-                break;
-        }
-    }
 }
